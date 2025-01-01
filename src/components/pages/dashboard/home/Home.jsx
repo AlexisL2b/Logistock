ecd68400510b60f0d4c997e8ae1754ed3ebd56ac
AlexisL2b <101928945+AlexisL2b@users.noreply.github.com>
2025-01-01 17:54:06 +0100

    ✨ F02>Ajout des différents composants pour la page dashboard>Création d'un composant From.

--- src/components/pages/dashboard/home/Home.jsx
+++ src/components/pages/dashboard/home/Home.jsx
@@ -1,6 +1,15 @@
+import { Box } from "@mui/material"
 import React from "react"
+import Form from "../../../reusable-ui/Form"
 
 export default function home() {
   console.log("Le composant Home est bien chargé")
-  return <div>home</div>
+  return (
+    <Box sx={{ backgroundColor: "red" }}>
+      <Form
+        action="submit"
+        onSubmit={console.log("Je viens de sumbit mon form")}
+      />
+    </Box>
+  )
 }
